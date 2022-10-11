d1104f4d3f9c33e42302fafb8e1b29c21d3a3a3f
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2022-10-11 09:46:21 -0300

    add/schema

--- src/server.js
+++ src/server.js
@@ -5,9 +5,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false  }))
 
-app.get("/", (req, res) => {
-	res.send("Test")
-})
 
 app.listen(3000, () => {
 	console.log("Server is runnig in: http://localhost:3000")
